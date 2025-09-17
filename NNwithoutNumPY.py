43675a1e755d993330af9577276dfcb82be2c444
RonenSherman <166041889+RonenSherman@users.noreply.github.com>
2025-09-17 13:05:09 -0700

    Learning function added for network with numpy and started network without numpy

---
+++ NNwithoutNumPY.py
@@ -0,0 +1,10 @@
+
+
+"""NN attempt without NumPY"""
+
+class SigmoidNeuron:
+
+    def __init__(self, x, weights, bias, learning_rate=0.1):
+        self.bias = float(bias)
+        self.learning_rate = learning_rate
+        self.weights =
